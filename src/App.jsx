7f97de00b91ce33640226fcb3ec30860c1e5452e
Pragati Ghosh <pragati25ghosh@gmail.com>
2025-05-31 23:44:27 +0530

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import './index.css';
 import Navbar from './components/Navbar/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Coin from './pages/Coin/Coin';
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/coin/:coinId' element={<Coin/>}></Route>
+      </Routes>
     </div>
   )
 }
